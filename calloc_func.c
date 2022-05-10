a9032a8c8b5140c5237c591fabaf2fd9ca36dad3
MateoArbini <mateoarbini1998@gmail.com>
2022-05-10 11:57:33 -0700

    calloc function

---
+++ calloc_func.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+/**
+ *_calloc - function that allocates memory for an array, using malloc
+ *@nmemb: elements
+ *@size: bytes of each element
+ *Return: p
+ **/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+	unsigned int i;
+
+	if (nmemb <= 0 || size <= 0)
+	{
+		return (NULL);
+	}
+
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < nmemb * size; i++)
+		*((char *)p + i) = 0;
+
+	return (p);
+}
